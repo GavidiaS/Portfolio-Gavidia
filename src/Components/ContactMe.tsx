af228cac87e7bcc844b9b1932892bd0dbd410381
GavidiaS <gavdia1302@gmail.com>
2023-08-28 15:08:32 -0500

    CV listo

--- src/Components/ContactMe.tsx
+++ src/Components/ContactMe.tsx
@@ -2,6 +2,7 @@ import { usePortfolioContext } from "../Context";
 import { FaDownload } from "react-icons/fa6";
 import { FaLinkedin, FaGithub, FaWhatsapp } from "react-icons/fa";
 import FormContact from "./FormContact";
+import cv from "../Resources/CV_GAVIDIA.pdf";
 
 export default function ContactMe() {
   const { data } = usePortfolioContext();
@@ -10,7 +11,7 @@ export default function ContactMe() {
       <h1>{data.invite}</h1>
       <FormContact />
       <div className="contact-me_links">
-        <a href="#">CV <FaDownload /></a>
+        <a href={cv} download={true}>CV <FaDownload /></a>
         <a href="https://www.linkedin.com/in/santiago-gavidia/" target="_blank"><FaLinkedin /></a>
         <a href="https://github.com/GavidiaS" target="_blank"><FaGithub /></a>
         <a href="https://wa.me/+51958251204" target="_blank"><FaWhatsapp /></a>
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -2,6 +2,7 @@ import { usePortfolioContext } from "../Context";
 import { FaDownload } from "react-icons/fa6";
 import { FaLinkedin, FaGithub, FaWhatsapp } from "react-icons/fa";
 import foto from '../Resources/Santiago-A.png';
+import cv from '../Resources/CV_GAVIDIA.pdf';
 
 const skills: string[] = ["HTML", "CSS", "Tailwind", "JavaScript", "React", "Redux", "Node.JS", "Express", "Sequelize", "PostgreSQL"];
 
@@ -19,7 +20,7 @@ export default function Profile() {
         </h2>
         <p>{data.description}</p>
         <div className="profile_contain-links">
-          <a href="#">CV <FaDownload /></a>
+          <a href={cv} download={true}>CV <FaDownload /></a>
           <a href="https://www.linkedin.com/in/santiago-gavidia/" target="_blank"><FaLinkedin /></a>
           <a href="https://github.com/GavidiaS" target="_blank"><FaGithub /></a>
           <a href="https://wa.me/+51958251204" target="_blank"><FaWhatsapp /></a>
